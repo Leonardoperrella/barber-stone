d9b081c011c9a7baacef888c5e2ea25e21ca2b1b
leonardo perrella <leonardo.perrella@yahoo.com.br>
2021-04-08 14:35:48 -0300

    alterada baseurl

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://kabit-api.herokuapp.com",
+  baseURL: "https://json-server-barberstone.herokuapp.com/",
 });
 
 export default api;
