6a64623211c1e2a76f804bc30c3e44084c18a839
leonardo perrella <leonardo.perrella@yahoo.com.br>
2021-04-09 13:25:00 -0300

    feat: adicionado pagina de login

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,7 +1,29 @@
-const LoginPage = () => {
-    return (
-        <div>LoginPage</div>
-    )
-}
+import GlobalContainer from "../components/GlobalContainer";
+import GlobalWrap from "../components/GlobalWrap";
+import Footer from "../components/Footer";
+import ButtonBackToHome from "../components/ButtonBackToHome";
+import { ButtonWrap, ElemP } from "./styles";
+import LinkToLogin from "../components/LinkToLogin";
+import FormLogin from "../components/FormLogin";
 
-export default LoginPage
+const BarberRegisterPage = () => {
+  return (
+    <>
+      <GlobalContainer>
+        <GlobalWrap>
+          <ButtonWrap>
+            <ButtonBackToHome />
+          </ButtonWrap>
+          <h1>Entrar</h1>
+          <FormLogin />
+          <ElemP>
+            Ja possui uma conta? <LinkToLogin>Entre Agora</LinkToLogin>
+          </ElemP>
+          <Footer />
+        </GlobalWrap>
+      </GlobalContainer>
+    </>
+  );
+};
+
+export default BarberRegisterPage;
