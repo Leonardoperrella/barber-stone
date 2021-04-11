490c1c26c5858f3e71ae0595125f502f2453b662
Leandro Arruda <nascimentoarruda123@gmail.com>
2021-04-11 20:37:32 -0300

    feat: :fire: Criada página de perfil da barbearia para desktop e mobile
    Criada página de perfil da barbearia para desktop e mobile

--- src/pages/BarberPerfilPage/styles.js
+++ src/pages/BarberPerfilPage/styles.js
@@ -1,5 +1,24 @@
 import styled from "styled-components";
 
+
+export const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 992 },
+      items: 3,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 992, min: 625 },
+      items: 2,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 625, min: 0 },
+      items: 1,
+      slidesToSlide: 1, // optional, default to 1.
+    },
+  };
+
 export const BodyPage = styled.div`
 display: flex;
 flex-direction: column;
@@ -7,13 +26,14 @@ box-sizing:border-box;
 align-items:center;
 `
 export const BgPerfil = styled.img`
-    width: 100vw;
+    width: 100%;
     height: 360px;
+    object-fit: cover;
 `
 export const ImgPerfil = styled.img`
     width: 300px;
     height: 300px;
-    margin: -50% auto 0 auto;
+    margin: -150px auto 0 auto;
 `
 export const Nome = styled.p`
     font-family: "Bebas Neue",Arial, Helvetica, sans-serif;
@@ -34,6 +54,9 @@ export const TextoDescritivo = styled.p`
     margin: 5px auto;
     color: #F7F4F3;
     text-align:center;
+    @media(min-width:900px) {
+        font-size:68px;
+    }
 `
 export const Icon = styled.img`
     width: 288px;
@@ -46,8 +69,8 @@ export const IconePequeno = styled.img`
     margin: 50px auto 15px auto;
 `
 export const Logo = styled.img`
-    width: 216px;
-    height: 148px;
+    max-width: 544px;
+    width: 70%;
     margin-top:60px;
 `
 export const Descricao = styled.p`
@@ -60,4 +83,14 @@ export const Descricao = styled.p`
     font-size: 32px;
     margin: 10px auto;
     color: #F7F4F3;
+`
+export const Container = styled.div`
+    padding: 10px;
+    cursor: grab;
+    width: 90vw;
+
+    & li {
+    display: flex;
+    justify-content: center;
+    }
 `
