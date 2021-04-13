01ef702bde41d7d2578c81b8a3e4b87c1935a62c
André Vieira <andreluizhillerv@gmail.com>
2021-04-13 10:17:14 -0300

    fix: Página de erro 404 revisada

--- src/pages/styles.js
+++ src/pages/styles.js
@@ -4,7 +4,8 @@ export const ErrorMsgDiv = styled.div`
   width: 100vw;
   height: 100vh;
   background-image: url("https://image.freepik.com/free-vector/404-error-page-found-illustration_199563-1.jpg");
-  background-repeat: repeat;
+  background-position: center;
+  background-size: contain;
 `;
 
 export const ButtonWrap = styled.button`
