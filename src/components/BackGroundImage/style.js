c8ff4fff74202a327a5dc6d34af67e6881f9c0f3
André Vieira <andreluizhillerv@gmail.com>
2021-04-18 10:47:07 -0300

    fix: Pequenas alterações

--- src/components/BackGroundImage/style.js
+++ src/components/BackGroundImage/style.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const BackGroundImageStyled = styled.div`
   @media (min-width: 1024px) {
-    background-image: ${({ image }) => `url(${image})`} no-repeat;
+    background-image: url(${({ image }) => image});
     position: fixed;
     width: 100%;
     height: 100vh;
